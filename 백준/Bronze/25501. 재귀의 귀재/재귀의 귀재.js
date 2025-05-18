647c79a304b503d2720d5c63cc0d468deefee4a8
bin778 <bin778@naver.com>
2025-05-18 20:54:18 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 188 ms, Memory: 11616 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.js
@@ -0,0 +1,21 @@
+let count = 0;
+
+function recursion(s, l, r) {
+    if (l === 0) count = 0;
+    count++;
+    if (l >= r) return 1;
+    else if (s[l] != s[r]) return 0;
+    else return recursion(s, l + 1, r - 1);
+}
+
+function isPalindrome(s) {
+    return recursion(s, 0, s.length - 1);
+}
+
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+input.shift();
+
+const result = input.map((x, i) => {
+    return `${isPalindrome(x)} ${count}`
+});
+console.log(result.join("\n"));
