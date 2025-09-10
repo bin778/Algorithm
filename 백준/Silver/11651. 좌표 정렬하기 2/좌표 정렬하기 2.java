225d2702de4f629bcc6388ed2efcf2db7677cc7e
bin778 <bin778@naver.com>
2025-09-10 15:10:32 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 636 ms, Memory: 58880 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -21,8 +21,10 @@ public class Main {
             else return x[1] - y[1];
         });
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            System.out.println(numArr[i][0] + " " + numArr[i][1]);
+            sb.append(numArr[i][0]).append(" ").append(numArr[i][1]).append("\n");
         }
+        System.out.println(sb);
     }
 }
