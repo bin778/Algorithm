28b7ce8258725b50770497ea3c568207a94e599d
bin778 <bin778@naver.com>
2025-09-11 00:26:40 +0900

    [level 1] Title: 폰켓몬, Time: 6.49 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
 
 ### 성능 요약
 
-메모리: 34.5 MB, 시간: 1.19 ms
+메모리: 78.2 MB, 시간: 6.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 18일 21:22:20
+2025년 09월 11일 00:26:38
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int maxCnt = nums.length / 2;
+        
+        Set<Integer> uniquePokemon = new HashSet<>();
+        for (int num : nums) {
+            uniquePokemon.add(num);
+        }
+        
+        int pokemonVC = uniquePokemon.size();
+        
+        return Math.min(pokemonVC, maxCnt);
+    }
+}
