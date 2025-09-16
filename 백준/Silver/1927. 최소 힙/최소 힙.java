0d0fd3999b149aac041d1fa979371f2606af56e9
bin778 <31675860+bin778@users.noreply.github.com>
2025-09-16 15:18:21 +0900

    [Silver II] Title: 최소 힙, Time: 1160 ms, Memory: 35696 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x > 0) {
+                pq.add(x);
+            } else if (x == 0) {
+                if (!pq.isEmpty()) {
+                    System.out.println(pq.poll());
+                } else {
+                    System.out.println(0);
+                }
+            }
+        }
+    }
+}
