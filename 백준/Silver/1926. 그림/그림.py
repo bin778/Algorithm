273838cafedff42f9fc67e91e53cc6984e35a2e5
bin778 <31675860+bin778@users.noreply.github.com>
2025-09-17 13:08:19 +0900

    [Silver I] Title: 그림, Time: 808 ms, Memory: 91452 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -6,31 +6,33 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 N, M = map(int, input().split())
+result, cnt = 0, 0
+num_list = []
 picture = [list(map(int, input().split())) for _ in range(N)]
 
-result = 0
-max_num = 0
-
 def dfs(x, y):
-    if not (0 <= x < N and 0 <= y < M and picture[x][y] == 1):
-        return 0
-    
-    picture[x][y] = 0
-    count = 1
+    global cnt
+    if picture[x][y] == 1:
+        picture[x][y] = 0
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        count += dfs(nx, ny)
-            
-    return count
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < N and 0 <= ny < M:
+                dfs(nx, ny)
+        cnt += 1
+        return cnt
+    else:
+        return 0
 
 for i in range(N):
     for j in range(M):
-        if picture[i][j] == 1:
+        if dfs(i, j): 
             result += 1
-            cur_num = dfs(i, j)
-            max_num = max(max_num, cur_num)
+            num_list.append(cnt)
+            cnt = 0
 
 print(result)
-print(max_num)
+if num_list: print(max(num_list))
+else: print(0)
