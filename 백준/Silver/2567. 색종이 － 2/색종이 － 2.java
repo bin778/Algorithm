d11454b604f595132e266b338517204419903edc
bin778 <31675860+bin778@users.noreply.github.com>
2025-09-19 16:10:04 +0900

    [Silver III] Title: 색종이 - 2, Time: 72 ms, Memory: 11628 KB -BaekjoonHub

---
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] paper = new int[100][100];
+        int result = 0;
+
+        for (int tc = 0; tc < N; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int i = x; i < x + 10; i++) {
+                for (int j = y; j < y + 10; j++) {
+                    if (i < 100 && j < 100) paper[i][j] = 1;
+                }
+            }
+        }
+
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (paper[i][j] == 1) {
+                    for (int k = 0; k < 4; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+
+                        if (nx < 0 || nx >= 100 || ny < 0 || ny >= 100 || paper[nx][ny] == 0) {
+                            result += 1;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
